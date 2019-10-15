1ac253bc7489957e65ff83b4127f4b5a1ce8eef9
Arjun Prasath Thamariselvan <arjunprasath.t@softcrylic.co.in>
2019-10-15 17:56:35 +0530

    9

--- Team/Arjun/app.js
+++ Team/Arjun/app.js
@@ -1,3 +1,13 @@
-let nooftickets = prompt("How many tickets you want?")
-let destination = prompt("Where do you want to travel?")
-let totalprice = 150
+let nooftickets = prompt("How many tickets you want?");
+let destination = prompt("Where do you want to travel?");
+let ticketprice = 150;
+const discount = ticketprice*(15/100);
+let totalprice = nooftickets*ticketprice;
+if (nooftickets>3){
+
+    alert("Final price:" + (totalprice-discount));
+
+else
+    alert("Final Price:" + totalprice);
+}
+
