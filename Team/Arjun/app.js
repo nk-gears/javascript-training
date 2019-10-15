179769a1e144244ef79ae75a5648a0836a3a8590
Arjun Prasath Thamariselvan <arjunprasath.t@softcrylic.co.in>
2019-10-15 18:38:18 +0530

    11

--- Team/Arjun/app.js
+++ Team/Arjun/app.js
@@ -4,11 +4,11 @@ let ticketprice = 100;
 let totalprice = nooftickets*ticketprice;
 const discount = totalprice*(15/100);
 if (nooftickets>3){
-    alert("TotalPrice:" + totalprice);
-    alert("Amount to be Paid after discount:" + (totalprice-discount));
+    alert(`TotalPrice: ${totalprice}`);
+    alert(`Amount to be Paid after discount: ${totalprice-discount}`);
 }
 else
 {
-    alert("Amount to be Paid:" + totalprice);
+    alert(`Amount to be Paid: ${totalprice}`);
 }
 
